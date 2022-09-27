b90affc67877cb58f7173d552ddffe51037475b4
Sam Meech-Ward <sam@meech-ward.me>
2022-09-27 11:53:38 -0700

    clean up article components

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -18,14 +18,6 @@ const languages = {
   vim: langVim
 }
 
-// import hljs from 'highlight.js'
-// import javascript from 'highlight.js/lib/languages/javascript'
-// import sql from 'highlight.js/lib/languages/sql'
-// import json from 'highlight.js/lib/languages/json'
-// hljs.registerLanguage('js', javascript)
-// hljs.registerLanguage('sql', sql)
-// hljs.registerLanguage('json', json)
-
 const h1 = (props) => <h1 {...props} className={props.className ?? "" + " text-5xl mt-14 mb-10 font-semibold"} />
 const h2 = (props) => <h2 {...props} className={props.className ?? "" + " text-4xl mt-12 mb-8 font-semibold"} />
 const h3 = (props) => <h3 {...props} className={props.className ?? "" + " text-3xl mt-10 mb-6 font-semibold"} />
@@ -46,39 +38,14 @@ function Instruction({ children }) {
 const Note = Instruction
 
 
-
 export default function Page({ source, rootURL }) {
   
   const [mdxSource, setMDXSource] = useState(null)
   
-  function img ({ src, alt }) {
-    if (src.startsWith('/')) {
-      src = rootURL + src
-    }
-    return (
-      <img src={src} alt={alt} />
-    )
-  }
-
-  
-
-  function Video({name, type}) {
-    return (
-      <video controls>
-        <source src={`${rootURL}/media/${name}`} type={type} />
-      </video>
-    )
-  }
-
-  function File({name, children}) {
-    return (
-      <p>
-        <a className="text-indigo-600 hover:text-indigo-500" href={`${rootURL}/files/${name}`}>{children}</a>
-      </p>
-    )
-  }
+  const img = ({ src, alt }) => src.startsWith('/') ? <img src={rootURL + src} alt={alt} /> : <img src={src} alt={alt} />
+  const File = ({name, children}) => <a className="text-indigo-600 hover:text-indigo-500" href={`${rootURL}/files/${name}`}>{children}</a>
     
-  const components = { h1, h2, h3, p, ul, ol, li, a, img, Instruction, Note, YouTube, Video, File }
+  const components = { h1, h2, h3, p, ul, ol, li, a, img, Instruction, Note, YouTube, File }
 
   useEffect(() => {
     (async () => {
@@ -100,10 +67,7 @@ export default function Page({ source, rootURL }) {
   return (
     <div className="max-w-3xl mx-auto">
       {mdxSource &&
-        <>
-          {/* <h1 className='text-4xl'>{mdxSource.frontmatter.title}</h1> */}
-          <MDXRemote {...mdxSource} components={components} />
-        </>
+        <MDXRemote {...mdxSource} components={components} />
       }
     </div>
   )
