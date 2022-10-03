a63b1e19a144f5a8762cb606f0ff7c17d8f1280c
Sam Meech-Ward <sam@meech-ward.me>
2022-10-03 08:22:54 -0700

    when a user logs in, store that user in dynamo

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,33 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter"
+import { dynamodb, tableName } from "../../../server/dynamo/dynamo"
+
 
 // https://docs.github.com/en/developers/apps/building-oauth-apps/creating-an-oauth-app
-
-export default NextAuth({
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     // ...add more providers here
   ],
-})
+  adapter: DynamoDBAdapter(dynamodb, {
+    tableName,
+    partitionKey: "pk",
+    sortKey: "sk",
+    indexName: "GSI1",
+    indexPartitionKey: "GSI1PK",
+    indexSortKey: "GSI1SK"
+  }),
+  secret: process.env.SECRET,
+}
+
+export default NextAuth(authOptions)
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { authOptions } from './auth/[...nextauth]'
+import { createUser } from "../../server/dynamo/queries"
+
+import { unstable_getServerSession } from "next-auth/next"
+
+
+export default async function handler(req, res) {
+  // next next auth user
+
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  if (!session) {
+    res.status(401).json({ error: "Unauthorized" })
+    return
+  }
+
+  const { username } = session.user
+
+
+  // createUser("sam")
+  res.status(200).json({ user: session.user })
 }
