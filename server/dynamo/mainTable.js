8601fd491b93e2d1a37c60e12fc0d79279a36b93
Sam Meech-Ward <sam@meech-ward.me>
2022-10-05 21:58:58 -0700

    Update aws env vars

--- server/dynamo/mainTable.js
+++ server/dynamo/mainTable.js
@@ -1,9 +1,9 @@
 import { makeTable } from './dynamo'
 
-const tableName = process.env.AWS_DYNAMO_TABLE_NAME
-const region = process.env.AWS_DYNAMO_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const tableName = process.env.MY_AWS_DYNAMO_TABLE_NAME
+const region = process.env.MY_AWS_DYNAMO_REGION
+const accessKeyId = process.env.MY_AWS_ACCESS_KEY
+const secretAccessKey = process.env.MY_AWS_SECRET_ACCESS_KEY
 
 const table = makeTable(tableName, region, accessKeyId, secretAccessKey)
 
