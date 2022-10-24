5713e61580aa61c6860aedf4abb81699c0697308
Sam Meech-Ward <sam@meech-ward.me>
2022-10-23 17:15:53 -0700

    respond with error message when error revalidating

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -16,6 +16,6 @@ export default async function handler(req, res) {
     console.error(err)
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send('Error revalidating', err.message)
   }
 }
