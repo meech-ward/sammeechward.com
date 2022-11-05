efcecf8a2156adac13ff9f90398ae45ff607f9d0
Sam Meech-Ward <sam@meech-ward.me>
2022-11-05 08:44:00 -0700

    Add social media share buttons

---
+++ components/ShareButtons/ShareButtons.stories.jsx
@@ -0,0 +1,12 @@
+import ShareButtons from './index'
+
+export default {
+  title: 'ShareButtons',
+  component: ShareButtons, 
+}
+
+export const Primary = () => (
+  <div className="pt-8 pb-10 lg:pt-12 lg:pb-14 mx-auto max-w-7xl px-2 h-screen">
+  <ShareButtons />
+  </div>
+)
