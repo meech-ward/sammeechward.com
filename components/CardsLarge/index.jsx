e3b125a23cc27cb2e7f47a10a6caaf86f693c75d
Sam Meech-Ward <sam@meech-ward.me>
2022-12-30 17:49:50 -0800

    add section headers on search page

--- components/CardsLarge/index.jsx
+++ components/CardsLarge/index.jsx
@@ -3,9 +3,11 @@ import Card from '../Card'
 
 import normalizeImageSize from '../../helpers/normalizeImageSize'
 
-export default function Cards({ posts, ImageComponent }) {
+import { twMerge } from 'tailwind-merge'
+
+export default function Cards({ posts, ImageComponent, className }) {
   return (
-    <div className="mx-auto mt-12 max-w-lg gap-10 lg:max-w-none grid lg:grid-cols-2">
+    <div className={twMerge("mx-auto mt-12 max-w-lg gap-10 lg:max-w-none grid lg:grid-cols-2", className)}>
       {posts.map((post) => {
           const imageSize = normalizeImageSize({ ...post.image, maxWidth: 1000 })
           const _post = { ...post }
