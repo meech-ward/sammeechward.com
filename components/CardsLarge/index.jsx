615783a952cb9bee29cf8a5e9e467d6cfee08a57
Sam Meech-Ward <sam@meech-ward.me>
2022-12-30 17:33:12 -0800

    make the playlist cards half width

--- components/CardsLarge/index.jsx
+++ components/CardsLarge/index.jsx
@@ -5,13 +5,13 @@ import normalizeImageSize from '../../helpers/normalizeImageSize'
 
 export default function Cards({ posts, ImageComponent }) {
   return (
-    <div className="mx-auto mt-12 max-w-lg gap-10 lg:max-w-none">
+    <div className="mx-auto mt-12 max-w-lg gap-10 lg:max-w-none grid lg:grid-cols-2">
       {posts.map((post) => {
           const imageSize = normalizeImageSize({ ...post.image, maxWidth: 1000 })
           const _post = { ...post }
           delete _post.date
           _post.extraData = `${_post.children.length} articles/videos`
-          return <Card className='my-10' ImageComponent={ImageComponent} post={_post} key={_post.id} imageSize={imageSize}></Card>
+          return <Card ImageComponent={ImageComponent} post={_post} key={_post.id} imageSize={imageSize}></Card>
         })}
     </div>
   )
