985b46ecd1a658c9d17bd6949504029f373d61ff
Sam <sam@meech-ward.me>
2023-02-08 15:59:38 -0800

    Add fragments to h2 elements

--- components/Article/index.jsx
+++ components/Article/index.jsx
@@ -1,131 +1,163 @@
-import NextImage from 'next/image'
+import NextImage from "next/image"
 
-import React, { useEffect, useState, Children } from 'react'
+import React, { useEffect, useState, Children } from "react"
 
-import { MDXRemote } from 'next-mdx-remote'
+import { MDXRemote } from "next-mdx-remote"
 
-import 'highlight.js/styles/stackoverflow-dark.css'
+import "highlight.js/styles/stackoverflow-dark.css"
 
-import YouTube from '../YouTube'
-import InteractiveParallelism from '../ArticleComponents/InteractiveParallelism'
-import SQLJoinsEditor from '../ArticleComponents/SQLJoinsEditor'
-import TabsWithSections from '../TabsWithSections'
-import ShareButtons from '../ShareButtons'
+import YouTube from "../YouTube"
+import InteractiveParallelism from "../ArticleComponents/InteractiveParallelism"
+import SQLJoinsEditor from "../ArticleComponents/SQLJoinsEditor"
+import TabsWithSections from "../TabsWithSections"
+import ShareButtons from "../ShareButtons"
 
-import Cards from '../Cards'
-import Card from '../Card'
+import Cards from "../Cards"
+import Card from "../Card"
 
-import GPTChat from '../ArticleComponents/ChatGPT/GPTChat'
-import GPTChatSection from '../ArticleComponents/ChatGPT/GPTChatSection'
+import GPTChat from "../ArticleComponents/ChatGPT/GPTChat"
+import GPTChatSection from "../ArticleComponents/ChatGPT/GPTChatSection"
 
-import { Note, Warning, Instruction, Error } from '../ArticleComponents/Blocks'
+import { Note, Warning, Instruction, Error } from "../ArticleComponents/Blocks"
 
-import normalizeImageSize from '../../helpers/normalizeImageSize'
-import { twMerge } from 'tailwind-merge'
+import normalizeImageSize from "../../helpers/normalizeImageSize"
+import { twMerge } from "tailwind-merge"
 
 function addPropToChildren(children, propName, propValue) {
-  return React.Children.map(children, child => {
+  return React.Children.map(children, (child) => {
     if (!React.isValidElement(child)) {
-      return child;
+      return child
     }
 
-    return React.cloneElement(child, { [propName]: propValue });
-  });
+    return React.cloneElement(child, { [propName]: propValue })
+  })
 }
 
-const h1 = (props) => <h1 {...{...props, gpt: undefined}} className={props.className ?? "" + (props.gpt ? "" : " sm:text-5xl text-4xl sm:mt-14 mt-10 sm:mb-10 mb-8 font-semibold")} />
-const h2 = (props) => <h2 {...{...props, gpt: undefined}} className={props.className ?? "" + (props.gpt ? "" : " sm:text-4xl text-3xl sm:mt-12 mt-8 sm:mb-8 mb-6 font-semibold")} />
-const h3 = (props) => <h3 {...{...props, gpt: undefined}} className={props.className ?? "" + (props.gpt ? "" : " sm:text-3xl text-lg  sm:mt-10 mt-6 sm:mb-6 mb-4 font-semibold")} />
-const p = (props) => <p {...{...props, gpt: undefined}} className={twMerge(props.className, (props.gpt ? "" : " sm:text-lg text-base sm:my-8 my-4 font-light"), (props.inline ? "inline" : ""))} />
-const ul = (props) => <ul {...{...props, gpt: undefined}} className={props.className ?? "" + (props.gpt ? " list-disc" : " sm:text-lg text-base sm:m-8  m-4 list-disc font-light")} />
-const ol = (props) => <ol {...{...props, gpt: undefined}} className={props.className ?? "" + (props.gpt ? " list-decimal" : " sm:text-lg text-base sm:m-8  m-4 list-decimal font-light")} />
-const li = (props) => <li {...{...props, gpt: undefined}} className={twMerge(props.className, "list-inside", (!props.gpt && "sm:mb-4 mb-2"))}>{addPropToChildren(props.children, 'inline', 'true')}</li>
-const a = (props) => <a {...{...props, gpt: undefined}} className={props.className ?? "" + (props.gpt ? "" : " text-indigo-600 hover:text-indigo-500 font-light")} />
-const pre = (props) => <pre {...{...props, gpt: undefined}} className={twMerge(props.className ?? "w-full" )}>{addPropToChildren(props.children, 'codeBlock', 'true')}</pre>
-const code = (props) => <code {...{...props, gpt: undefined}} className={twMerge(props.className, (props.codeBlock ? "" : " inline"), (props.gpt ? "gpt" : ""))  }/>
+const urlEncodeFragment = (fragment) =>
+  fragment
+    .replace(/ /g, "-")
+    .replace(/[^a-zA-Z0-9-]/g, "")
+    .toLowerCase()
 
-function urlForLocalFile({path, dirUrl}) {
-  if (path.startsWith('/images')) {
-    return new URL(path.replace('/images/', 'images/'), dirUrl+"/").href
+const h1 = (props) => <h1 {...{ ...props, gpt: undefined }} className={props.className ?? "" + (props.gpt ? "" : " sm:text-5xl text-4xl sm:mt-14 mt-10 sm:mb-10 mb-8 font-semibold")} />
+const h2 = (props) => {
+  const fragment = urlEncodeFragment(props.children.toString())
+  return (
+    <a href={`#${fragment}`}>
+      <h2 id={fragment} {...{ ...props, gpt: undefined }} className={props.className ?? "" + (props.gpt ? "" : " sm:text-4xl text-3xl sm:mt-12 mt-8 sm:mb-8 mb-6 font-semibold")} />
+    </a>
+  )
+}
+const h3 = (props) => <h3 {...{ ...props, gpt: undefined }} className={props.className ?? "" + (props.gpt ? "" : " sm:text-3xl text-lg  sm:mt-10 mt-6 sm:mb-6 mb-4 font-semibold")} />
+const p = (props) => <p {...{ ...props, gpt: undefined }} className={twMerge(props.className, props.gpt ? "" : " sm:text-lg text-base sm:my-8 my-4 font-light", props.inline ? "inline" : "")} />
+const ul = (props) => <ul {...{ ...props, gpt: undefined }} className={props.className ?? "" + (props.gpt ? " list-disc" : " sm:text-lg text-base sm:m-8  m-4 list-disc font-light")} />
+const ol = (props) => <ol {...{ ...props, gpt: undefined }} className={props.className ?? "" + (props.gpt ? " list-decimal" : " sm:text-lg text-base sm:m-8  m-4 list-decimal font-light")} />
+const li = (props) => (
+  <li {...{ ...props, gpt: undefined }} className={twMerge(props.className, "list-inside", !props.gpt && "sm:mb-4 mb-2")}>
+    {addPropToChildren(props.children, "inline", "true")}
+  </li>
+)
+const a = (props) => <a {...{ ...props, gpt: undefined }} className={props.className ?? "" + (props.gpt ? "" : " text-indigo-600 hover:text-indigo-500 font-light")} />
+const pre = (props) => (
+  <pre {...{ ...props, gpt: undefined }} className={twMerge(props.className ?? "w-full")}>
+    {addPropToChildren(props.children, "codeBlock", "true")}
+  </pre>
+)
+const code = (props) => <code {...{ ...props, gpt: undefined }} className={twMerge(props.className, props.codeBlock ? "" : " inline", props.gpt ? "gpt" : "")} />
+
+function urlForLocalFile({ path, dirUrl }) {
+  if (path.startsWith("/images")) {
+    return new URL(path.replace("/images/", "images/"), dirUrl + "/").href
   }
-  if (path.startsWith('/files')) {
-    return new URL(path.replace('/files/', 'files/'), dirUrl+"/").href
+  if (path.startsWith("/files")) {
+    return new URL(path.replace("/files/", "files/"), dirUrl + "/").href
   }
-  if (path.startsWith('/assets')) {
-    return new URL(path.replace('/assets/', 'assets/'), dirUrl+"/").href
+  if (path.startsWith("/assets")) {
+    return new URL(path.replace("/assets/", "assets/"), dirUrl + "/").href
   }
   return url
 }
 
-
-
 export default function Page({ mdxSource, dirUrl, getPostBySlug, ImageComponent = NextImage, url, title, urlShort, ...props }) {
-
-  function PostCard({slug}) {
+  function PostCard({ slug }) {
     const [post, setPost] = useState(null)
     useEffect(() => {
-      getPostBySlug(slug)
-        .then(setPost)
+      getPostBySlug(slug).then(setPost)
     }, [slug])
     if (!post) return null
     // return <Cards posts={[post]} />
     const imageSize = normalizeImageSize({ ...post.image, maxHeight: 192 * 2 })
-    return <div className='mb-10 max-w-sm mx-auto'><Card  ImageComponent={ImageComponent} post={post} imageSize={imageSize}></Card></div>
-  }
-
-  function Tabs({children}) {
-    const tabs = Children.map(children, (child) => ({...child.props}))
-    tabs[0].current = true
     return (
-      <TabsWithSections tabs={tabs} />
+      <div className="mb-10 max-w-sm mx-auto">
+        <Card ImageComponent={ImageComponent} post={post} imageSize={imageSize}></Card>
+      </div>
     )
   }
 
-  function Tab({children, name}) {
+  function Tabs({ children }) {
+    const tabs = Children.map(children, (child) => ({ ...child.props }))
+    tabs[0].current = true
+    return <TabsWithSections tabs={tabs} />
+  }
+
+  function Tab({ children, name }) {
     return <div>Tab: name</div>
   }
 
-  const img = ({ src, props }) => (src.startsWith('/images') || src.startsWith("/assets")) ? <img src={urlForLocalFile({path: props.src, dirUrl})} {...props} /> : <img src={src} {...props} />
-  const Image = ({src, ...props}) => {
-    return (src.startsWith('/images') || src.startsWith("/assets")) ? <NextImage {...props} src={urlForLocalFile({path: src, dirUrl})} /> : <img {...props} src={src} />
+  const img = ({ src, props }) => (src.startsWith("/images") || src.startsWith("/assets") ? <img src={urlForLocalFile({ path: props.src, dirUrl })} {...props} /> : <img src={src} {...props} />)
+  const Image = ({ src, ...props }) => {
+    return src.startsWith("/images") || src.startsWith("/assets") ? <NextImage {...props} src={urlForLocalFile({ path: src, dirUrl })} /> : <img {...props} src={src} />
   }
-  const File = ({ path, children }) => <a className="text-indigo-600 hover:text-indigo-500" href={urlForLocalFile({path, dirUrl})}>{children}</a>
+  const File = ({ path, children }) => (
+    <a className="text-indigo-600 hover:text-indigo-500" href={urlForLocalFile({ path, dirUrl })}>
+      {children}
+    </a>
+  )
 
   const AutoPlayVideo = ({ path, children }) => {
     // auto play video without sound or controls
     return (
-      <div className="relative w-full h-0" style={{paddingBottom: "56.25%"}}>
+      <div className="relative w-full h-0" style={{ paddingBottom: "56.25%" }}>
         <video {...children} className="absolute top-0 left-0 w-full h-full" autoPlay muted loop playsInline>
-          <source src={urlForLocalFile({path, dirUrl})} type="video/mp4" />
+          <source src={urlForLocalFile({ path, dirUrl })} type="video/mp4" />
         </video>
       </div>
     )
   }
 
-  const components = { 
-    h1, h2, h3, p, ul, ol, li, a, img, pre, code,
-    Image, Note, Warning, Instruction, Error,
-    Tabs, Tab,
-    PostCard, 
-    YouTube, File, AutoPlayVideo,
-    InteractiveParallelism, SQLJoinsEditor,
-    GPTChat, GPTChatSection
+  const components = {
+    h1,
+    h2,
+    h3,
+    p,
+    ul,
+    ol,
+    li,
+    a,
+    img,
+    pre,
+    code,
+    Image,
+    Note,
+    Warning,
+    Instruction,
+    Error,
+    Tabs,
+    Tab,
+    PostCard,
+    YouTube,
+    File,
+    AutoPlayVideo,
+    InteractiveParallelism,
+    SQLJoinsEditor,
+    GPTChat,
+    GPTChatSection,
   }
 
   return (
     <div className="break-words">
-      <ShareButtons 
-      className='float-right mb-2'
-      url={url}
-      title={title}
-      urlShort={urlShort}
-      />
-      <div className='clear-both'>
-      {mdxSource &&
-        <MDXRemote {...mdxSource} components={components} />
-      }
-      </div>
+      <ShareButtons className="float-right mb-2" url={url} title={title} urlShort={urlShort} />
+      <div className="clear-both">{mdxSource && <MDXRemote {...mdxSource} components={components} />}</div>
     </div>
   )
 }
-
