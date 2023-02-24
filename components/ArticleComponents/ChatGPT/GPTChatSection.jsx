515a72a0836399238f73e2d79f2194c1fe909077
Sam Meech-Ward <sam@sam.local>
2023-02-24 15:39:47 -0800

    Remove chatgpt buttons

--- components/ArticleComponents/ChatGPT/GPTChatSection.jsx
+++ components/ArticleComponents/ChatGPT/GPTChatSection.jsx
@@ -124,23 +124,6 @@ export default function GPTChatSection({ role, children }) {
             <div className="min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap">{_child}</div>
           </div>
           <div className="text-gray-400 flex self-end lg:self-center justify-center mt-2 gap-3 md:gap-4 lg:gap-1 lg:absolute lg:top-0 lg:translate-x-full lg:right-0 lg:mt-0 lg:pl-2 visible">
-            <button className="p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400 md:invisible md:group-hover:visible">
-              <svg
-                stroke="currentColor"
-                fill="none"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="h-4 w-4"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-              </svg>
-            </button>
           </div>
           <div className="flex justify-between"></div>
         </div>
