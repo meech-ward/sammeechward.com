28212d6d79274852b9c4766bab0024a3dd2ae6fd
Sam Meech-Ward <sam@sam.local>
2023-02-24 11:57:34 -0800

    Make it more accessible friendly

--- components/ShareButtons/index.jsx
+++ components/ShareButtons/index.jsx
@@ -6,6 +6,7 @@ export default function ShareActions({ className, url, title, urlShort }) {
     <div className={twMerge('flex flex-col justify-center items-center', className)}>
       <div className="flex w-full justify-between">
         <a
+          aria-label="Share on Twitter"
           href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(title)}&url=${encodeURIComponent(url)}`}
           target="_blank"
           rel="noopener noreferrer"
@@ -17,6 +18,7 @@ export default function ShareActions({ className, url, title, urlShort }) {
           </svg>
         </a>
         <a
+          aria-label="Share on Facebook"
           href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`}
           target="_blank"
           rel="noopener noreferrer"
@@ -28,6 +30,7 @@ export default function ShareActions({ className, url, title, urlShort }) {
           </svg>
         </a>
         <a
+          aria-label="Share on LinkedIn"
           href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}`}
           target="_blank"
           rel="noopener noreferrer"
@@ -38,7 +41,9 @@ export default function ShareActions({ className, url, title, urlShort }) {
             <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 16 12 C 14.35499 12 13 13.35499 13 15 C 13 16.64501 14.35499 18 16 18 C 17.64501 18 19 16.64501 19 15 C 19 13.35499 17.64501 12 16 12 z M 16 14 C 16.564129 14 17 14.435871 17 15 C 17 15.564129 16.564129 16 16 16 C 15.435871 16 15 15.564129 15 15 C 15 14.435871 15.435871 14 16 14 z M 14 19 A 1.0001 1.0001 0 0 0 13 20 L 13 35 A 1.0001 1.0001 0 0 0 14 36 L 18 36 A 1.0001 1.0001 0 0 0 19 35 L 19 20 A 1.0001 1.0001 0 0 0 18 19 L 14 19 z M 22 19 A 1.0001 1.0001 0 0 0 21 20 L 21 35 A 1.0001 1.0001 0 0 0 22 36 L 26 36 A 1.0001 1.0001 0 0 0 27 35 L 27 27.5 C 27 26.120455 28.120455 25 29.5 25 C 30.879545 25 32 26.120455 32 27.5 L 32 30 L 32 35 A 1.0001 1.0001 0 0 0 33 36 L 37 36 A 1.0001 1.0001 0 0 0 38 35 L 38 26.5 C 38 22.36961 34.63039 19 30.5 19 C 29.213528 19 28.059744 19.41615 27 19.990234 A 1.0001 1.0001 0 0 0 26 19 L 22 19 z M 15 21 L 17 21 L 17 34 L 15 34 L 15 21 z M 23 21 L 25 21 L 25 21.816406 A 1.0001 1.0001 0 0 0 26.693359 22.537109 C 27.684186 21.585305 29.016683 21 30.5 21 C 33.54961 21 36 23.45039 36 26.5 L 36 34 L 34 34 L 34 30 L 34 27.5 C 34 25.029545 31.970455 23 29.5 23 C 27.029545 23 25 25.029545 25 27.5 L 25 34 L 23 34 L 23 21 z" />
           </svg>
         </a>
-      <button className='flex items-center justify-center h-8 w-8 rounded-full  text-gray-400 hover:text-gray-500 mx-2' onClick={() => navigator.clipboard.writeText(urlShort || url)}><LinkIcon className="h-7 w-7 mr-2" aria-hidden="true" /></button>
+        <button
+          aria-label="Copy link"
+          className='flex items-center justify-center h-8 w-8 rounded-full  text-gray-400 hover:text-gray-500 mx-2' onClick={() => navigator.clipboard.writeText(urlShort || url)}><LinkIcon className="h-7 w-7 mr-2" aria-hidden="true" /></button>
       </div>
     </div>
   )
