cd49e41ff5f0f4a26b1792839ecf0d5a5014ceeb
Sam Meech-Ward <sam@sam.local>
2023-02-24 22:24:47 -0800

    remove id from select

--- components/Tabs/index.jsx
+++ components/Tabs/index.jsx
@@ -2,25 +2,24 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Tabs({tabs, onChange}) {
+export default function Tabs({ tabs, onChange }) {
   return (
     <div>
       <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
+        <label className="sr-only">
           Select a tab
+          {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+          <select
+            onChange={(e) => onChange(e.target.value)}
+            name="tabs"
+            className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            defaultValue={tabs.find((tab) => tab.current).name}
+          >
+            {tabs.map((tab) => (
+              <option key={tab.name}>{tab.name}</option>
+            ))}
+          </select>
         </label>
-        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-        <select
-          onChange={(e) => onChange(e.target.value)}
-          id="tabs"
-          name="tabs"
-          className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          defaultValue={tabs.find((tab) => tab.current).name}
-        >
-          {tabs.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
       </div>
       <div className="hidden sm:block">
         <div className="border-b border-gray-200">
