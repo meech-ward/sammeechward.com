c1257045abd46917a37127ab8ecf52be83a26242
Sam Meech-Ward <sam@sam.local>
2023-04-07 11:10:46 -0700

    Add google adsense

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,6 +8,7 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
+import Script from 'next/script';
 TimeAgo.setDefaultLocale(en.locale)
 TimeAgo.addLocale(en)
 
@@ -17,6 +18,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <GoogleAnalytics trackPageViews />
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7566221825946498"
+      crossorigin="anonymous"></Script>
       <SiteNavigation />
       <Component {...pageProps} />
     </SessionProvider>
