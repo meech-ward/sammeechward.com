638a5bb51ea9a3874a44ba3c198cc4541815649b
Sam Meech-Ward <sam@sam.local>
2023-04-08 09:33:10 -0700

    update the scripts for ads

--- components/Ads/index.jsx
+++ components/Ads/index.jsx
@@ -10,10 +10,20 @@ export function InArticle() {
   if (!loaded) {
     return <></>
   }
-  return process.env.NODE_ENV !== 'production' ? (
+  return process.env.NODE_ENV !== "production" ? (
     <h1>Ad</h1>
   ) : (
-    <ins className="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-7566221825946498" data-ad-slot="4599631467"></ins>
+    <>
+      <ins
+        className="adsbygoogle"
+        style="display:block; text-align:center;"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-7566221825946498"
+        data-ad-slot="4599631467"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </>
   )
 }
 
@@ -27,9 +37,12 @@ export function InFeed() {
   if (!loaded) {
     return <></>
   }
-  return process.env.NODE_ENV !== 'production' ? (
+  return process.env.NODE_ENV !== "production" ? (
     <h1>Ad</h1>
   ) : (
-    <ins className="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-5j+c6-1t-7m+w8" data-ad-client="ca-pub-7566221825946498" data-ad-slot="6633895726"></ins>
+    <>
+      <ins className="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-5j+c6-1t-7m+w8" data-ad-client="ca-pub-7566221825946498" data-ad-slot="6633895726"></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </>
   )
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,28 +1,32 @@
-import '../styles/globals.css'
-import SiteNavigation from '../components/SiteNavigation'
+import "../styles/globals.css"
+import SiteNavigation from "../components/SiteNavigation"
 
 import { SessionProvider } from "next-auth/react"
 
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
-
-import TimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
-import Script from 'next/script';
+import TimeAgo from "javascript-time-ago"
+import en from "javascript-time-ago/locale/en"
+import Script from "next/script"
 TimeAgo.setDefaultLocale(en.locale)
 TimeAgo.addLocale(en)
 
-
 function MyApp({ Component, pageProps }) {
-
   return (
     <SessionProvider session={pageProps.session}>
       <GoogleAnalytics trackPageViews />
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7566221825946498"
-      crossorigin="anonymous"></Script>
+      <Script
+        id="ca-pub-7566221825946498"
+        async
+        onError={(e) => {
+          console.error("Script failed to load", e)
+        }}
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7566221825946498"
+        crossorigin="anonymous"
+      ></Script>
       <SiteNavigation />
       <Component {...pageProps} />
-      <Script id="adsbygoogle-script">(adsbygoogle = window.adsbygoogle || []).push({});</Script>
     </SessionProvider>
   )
 }
