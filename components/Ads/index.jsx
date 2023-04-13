7e87bb2b9b844959ed701d9640c6fcbf5b006681
sam <sam@meech-ward.me>
2023-04-12 20:03:52 -0700

    update all the ads

--- components/Ads/index.jsx
+++ components/Ads/index.jsx
@@ -1,61 +1,9 @@
 import { useEffect, useState, useRef } from "react"
 
-export function InArticle() {
+function Ad(props) {
   const [loaded, setLoaded] = useState(false)
-
-  useEffect(() => {
-    setLoaded(true)
-  }, [])
-
-  if (!loaded) {
-    return <></>
-  }
-  return (
-    <>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block", textAlign: "center" }}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-7566221825946498"
-        data-ad-slot="4599631467"
-        data-adtest={process.env.NODE_ENV !== "production"}
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-    </>
-  )
-}
-
-export function InDisplay() {
-  const [loaded, setLoaded] = useState(false)
-
-  useEffect(() => {
-    setLoaded(true)
-  }, [])
-
-  if (!loaded) {
-    return <></>
-  }
-  return (
-    <>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-layout="in-article"
-        data-ad-format="auto"
-        data-ad-client="ca-pub-7566221825946498"
-        data-ad-slot="2975875836"
-        data-full-width-responsive="true"
-        data-adtest={process.env.NODE_ENV !== "production"}
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-    </>
-  )
-}
-
-export function InFeed() {
-  const [loaded, setLoaded] = useState(false)
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
+  const [notPushed, setNotPushed] = useState(true)
   const elementRef = useRef(null)
 
   useEffect(() => {
@@ -70,51 +18,87 @@ export function InFeed() {
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            setIsVisible(true);
+            setIsVisible(true)
           } else {
-            setIsVisible(false);
+            setIsVisible(false)
           }
-        });
+        })
       },
       {
         root: null,
-        rootMargin: '0px',
+        rootMargin: "0px",
         threshold: 0.1, // Adjust this value to control when the element is considered visible.
       }
-    );
+    )
 
     if (elementRef.current) {
-      observer.observe(elementRef.current);
+      observer.observe(elementRef.current)
     }
 
     return () => {
       if (elementRef.current) {
-        observer.unobserve(elementRef.current);
+        observer.unobserve(elementRef.current)
       }
-    };
-  }, [loaded]);
+    }
+  }, [loaded])
 
   useEffect(() => {
-    if (isVisible) {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    if (isVisible && notPushed) {
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+      setNotPushed(false)
     }
-  }, [isVisible]);
+  }, [isVisible, pushed])
 
   if (!loaded) {
     return <></>
   }
   return (
     <>
-      <ins
-        ref={elementRef}
-        data-adtest={process.env.NODE_ENV !== "production"}
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-format="fluid"
-        data-ad-layout-key="-5j+c6-1t-7m+w8"
-        data-ad-client="ca-pub-7566221825946498"
-        data-ad-slot="6633895726"
-      ></ins>
+      <ins ref={elementRef} {...props}></ins>
     </>
+  )
+}
+
+export function InArticle() {
+  return (
+    <Ad
+      className="adsbygoogle"
+      style={{ display: "block", textAlign: "center" }}
+      data-ad-layout="in-article"
+      data-ad-format="fluid"
+      data-ad-client="ca-pub-7566221825946498"
+      data-ad-slot="4599631467"
+      data-adtest={process.env.NODE_ENV !== "production"}
+    />
+  )
+}
+
+export function InDisplay() {
+  return (
+    <Ad
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-layout="in-article"
+      data-ad-format="auto"
+      data-ad-client="ca-pub-7566221825946498"
+      data-ad-slot="2975875836"
+      data-full-width-responsive="true"
+      data-adtest={process.env.NODE_ENV !== "production"}
+    />
+  )
+}
+
+export function InFeed() {
+  return (
+    <Ad
+      ref={elementRef}
+      data-adtest={process.env.NODE_ENV !== "production"}
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-format="fluid"
+      data-ad-layout-key="-5j+c6-1t-7m+w8"
+      data-ad-client="ca-pub-7566221825946498"
+      data-ad-slot="6633895726"
+    />
   )
 }
--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -57,7 +57,7 @@ export default function Articles(props) {
       </Head>
       <div className="pt-8 pb-10 lg:pt-12 lg:pb-14 mx-auto max-w-7xl px-2">
         <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>Videos, Articles, and Tutorials</h1>
-        <Cards posts={entities} ImageComponent={ImageComponent} showAds={props.adsEnabled ? {every: 25, at: 4} : null} />
+        <Cards posts={entities} ImageComponent={ImageComponent} showAds={props.adsEnabled ? {every: 20, at: 4} : null} />
         {lastEvaluatedKey &&
           <div className="pt-8 pb-10 lg:pt-12 lg:pb-14 mx-auto max-w-7xl px-2">
             <button
